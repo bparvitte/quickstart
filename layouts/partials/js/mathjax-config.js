3f78e559796db1f65c20a2bbb2635398a0347b35
Bertrand Parvitte <bertrand@iMac-de-Bertrand.local>
2021-06-09 15:34:24 +0200

    suite test mathjax

---
+++ layouts/partials/js/mathjax-config.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+  loader: {load: ['[tex]/mhchem']},
+  tex: {packages: {'[+]': ['mhchem']}}
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
